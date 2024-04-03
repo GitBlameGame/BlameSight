9139d62a8e74bda1ff2c5c6364a480fda2ffde00
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-04-03 19:43:50 +0200

    Added health check

---
+++ Backend/BlameSightBackend/BlameSightBackend/Controllers/HealthCheck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlameSightBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheck : Controller
+    {
+        [HttpGet]
+        public IActionResult checkHealth()
+        {
+            return Ok("Health GOOD");
+        }
+    }
+}
