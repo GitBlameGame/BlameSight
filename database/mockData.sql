3d2a215e65cd69c951d6ffaed78fe7eb8b10bc97
Karl <karl.etsebeth@bbd.co.za>
2024-04-08 20:38:50 +0200

    Mock Data hotfix

--- database/mockData.sql
+++ database/mockData.sql
@@ -10,11 +10,11 @@ insert into users (user_name) values
 
 --changeset karl:dml:mockData:urgency_descriptors
 insert into urgency_descriptors (urgency_descriptor_id, urgency_descriptor_name) values
-('Formatting Issues ☕'),
-('Mini Bug 🐞'),
-('Medium Priority'),
-('Urgent🚨'),
-('PROD on Fire! 🔥');
+(1,'Formatting Issues ☕'),
+(2,'Mini Bug 🐞'),
+(3,'Medium Priority'),
+(4,'Urgent🚨'),
+(5,'PROD on Fire! 🔥');
 --rollback DELETE FROM "urgency_descriptors";
 
 --changeset karl:dml:mockData:repo_owners
