1b0c2464cbab65f71d942ffd40b40af66bbea486
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-04-08 19:29:21 +0200

    🐍

--- database/tableCreates.sql
+++ database/tableCreates.sql
@@ -1,26 +1,26 @@
 --changeset karl:ddl:createTable:Users
-CREATE TABLE Users(
+CREATE TABLE users(
 	user_id SERIAL PRIMARY KEY NOT NULL,
 	user_name VARCHAR(39) NOT NULL UNIQUE
 );
 --rollback DROP TABLE "Users";
 
 --changeset karl:ddl:createTable:UrgencyDescriptors
-CREATE TABLE UrgencyDescriptors(
+CREATE TABLE urgency_descriptors(
 	urgency_descriptor_id INT PRIMARY KEY NOT NULL,
 	urgency_descriptor_name VARCHAR(30) NOT NULL UNIQUE
 );
 --rollback DROP TABLE "UrgencyDescriptors";
 
 --changeset karl:ddl:createTable:RepoOwners
-CREATE TABLE RepoOwners(
+CREATE TABLE repo_owners(
 	repo_owner_id SERIAL PRIMARY KEY NOT NULL,
 	repo_owner_name VARCHAR(1000) NOT NULL UNIQUE
 );
 --rollback DROP TABLE "RepoOwners";
 
 --changeset karl:ddl:createTable:Repos
-CREATE TABLE Repos(
+CREATE TABLE repos(
 	repo_id SERIAL PRIMARY KEY NOT NULL,
 	repo_owner_id INT NOT NULL,
 	repo_name VARCHAR(1000) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE Repos(
 --rollback DROP TABLE "Repos";
 
 --changeset karl:ddl:createTable:Blames
-CREATE TABLE Blames(
+CREATE TABLE blames(
 	blame_id SERIAL PRIMARY KEY NOT NULL,
 	blamer_id INT NOT NULL,
 	blamed_id INT NOT NULL,
