f7b4ea223f927ef4e9e06876d1c1bcb5b18dd292
Karl <karl.etsebeth@bbd.co.za>
2024-04-08 20:58:49 +0200

    Final DB hotfix: Fix rollback naming

--- database/tableAlters.sql
+++ database/tableAlters.sql
@@ -1,29 +1,29 @@
---changeset karl:ddl:Repos:fk_repo_owner
+--changeset karl:ddl:repos:fk_repo_owner
 ALTER TABLE repos
 ADD CONSTRAINT fk_repo_owner
 FOREIGN KEY (repo_owner_id) REFERENCES repo_owners(repo_owner_id);
---rollback ALTER TABLE "Repos" DROP CONSTRAINT fk_repo_owner
+--rollback ALTER TABLE "repos" DROP CONSTRAINT fk_repo_owner
 
---changeset karl:ddl:Blames:fk_blamer
+--changeset karl:ddl:blames:fk_blamer
 ALTER TABLE blames
 ADD CONSTRAINT fk_blamer
 FOREIGN KEY (blamer_id) REFERENCES users(user_id);
---rollback ALTER TABLE "Blames" DROP CONSTRAINT fk_blamer
+--rollback ALTER TABLE "blames" DROP CONSTRAINT fk_blamer
 
---changeset karl:ddl:Blames:fk_urgency_descriptor
+--changeset karl:ddl:blames:fk_urgency_descriptor
 ALTER TABLE blames
 ADD CONSTRAINT fk_urgency_descriptor
 FOREIGN KEY (urgency_descriptor_id) REFERENCES urgency_descriptors(urgency_descriptor_id);
---rollback ALTER TABLE "Blames" DROP CONSTRAINT fk_urgency_descriptor
+--rollback ALTER TABLE "blames" DROP CONSTRAINT fk_urgency_descriptor
 
---changeset karl:ddl:Blames:fk_repo
+--changeset karl:ddl:blames:fk_repo
 ALTER TABLE blames
 ADD CONSTRAINT fk_repo
 FOREIGN KEY (repo_id) REFERENCES repos(repo_id);
---rollback ALTER TABLE "Blames" DROP CONSTRAINT fk_repo
+--rollback ALTER TABLE "blames" DROP CONSTRAINT fk_repo
 
---changeset karl:ddl:Blames:fk_blamed
+--changeset karl:ddl:blames:fk_blamed
 ALTER TABLE blames
 ADD CONSTRAINT fk_blamed
 FOREIGN KEY (blamed_id) REFERENCES users(user_id);
---rollback ALTER TABLE "Blames" DROP CONSTRAINT fk_blamed
+--rollback ALTER TABLE "blames" DROP CONSTRAINT fk_blamed
